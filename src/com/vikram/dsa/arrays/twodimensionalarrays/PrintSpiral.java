374e050d8702b7e7f9e51666ac0cb36f1974e984
vikram <vikramsinghchauhan1881999@gmail.com>
2025-01-31 16:27:35 +0530

    Print Spiral Medium edge case

--- src/com/vikram/dsa/arrays/twodimensionalarrays/PrintSpiral.java
+++ src/com/vikram/dsa/arrays/twodimensionalarrays/PrintSpiral.java
@@ -5,7 +5,9 @@ public class PrintSpiral {
     public static void main( String[] args ) {
         PrintSpiral printSpiral = new PrintSpiral();
         int[][] arr = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13, 14, 15, 16}};
+        int[][] arr1 = { {1,2,3,4, 5}, {6,7,8,9, 10}, {11,12, 13, 14, 15} ,{ 16, 17,18,19,20}, {21, 22, 23, 24, 25}};
         printSpiral.printSpiral(arr);
+        printSpiral.printSpiral(arr1);
     }
 
     public void printSpiral(int[][] arr){
@@ -37,7 +39,7 @@ public class PrintSpiral {
         }
 
         if( n%2 == 1 ) {
-            System.out.print(arr[n/2][n/2]);
+            System.out.print(arr[i][j]);
         }
     }
 }
